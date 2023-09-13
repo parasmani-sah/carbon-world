0dc1a11d648d83da4ba22ee221e65bbb58d94660
Paras sah <47777250+parassah@users.noreply.github.com>
2023-09-13 10:38:07 +0545

    Changes in Next Config file to export3

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
 
 module.exports = nextConfig
 
+// Add any other routes you want to export
+module.exports = {
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' }, 
+      };
+    },
+  };
+  
 // next.config.js
 const withVideos = require('next-videos')
 
